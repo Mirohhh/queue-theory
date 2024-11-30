9345e89eb9e1b62d7a570d3a3dbea85608853d82
Mirohhh <omar.h.shabana@gmail.com>
2024-11-30 21:40:55 +0200

    Add logic for calculating the outputs for M/M/c model.

--- src/index.js
+++ src/index.js
@@ -20,30 +20,41 @@ function roundToTwo(number) {
     return Math.round((number + Number.EPSILON) * 100) / 100;
 }
 
-function detModel(s, b, lm, m) {
-    let ret;
-    if (s == 1 && b == 0) {
-        ret = calcMM1(lm, m);
-    } else if (s == 1 && b > 0) {
-        ret = calcMM1K(b, lm, m);
-    } else if (s > 1 && b == 0) {
-        ret = calcMMC();
-    } else if (s > 1 && b > 0) {
-        ret = calcMMCK();
-    } else {
-        ret = "Please enter valid inputs.";
+function factorial(num) {
+    if (num < 0) {
+        throw new Error("Factorial is not defined for negative numbers.");
     }
+    if (num === 0 || num === 1) {
+        return 1;
+    }
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
 
-    return ret;
+function detModel(s, b, lm, m) {
+    if (s == 1 && b == 0) {
+        return calcMM1(lm, m);
+    } else if (s == 1 && b > 0) {
+        return calcMM1K(b, lm, m);
+    } else if (s > 1 && b == 0) {
+        return calcMMC(s, lm, m);
+    } else if (s > 1 && b > 0) {
+        return calcMMCK();
+    } 
+    
+    return "Please enter valid inputs.";
 }
 
 function calcMM1(lmd, mui) {
-    let w = roundToTwo(1 / (mui - lmd));
-    let wq = roundToTwo(lmd / (mui * (mui - lmd)));
-    let l = roundToTwo(lmd * w);
-    let lq = roundToTwo(lmd * wq);
+    let w = 1 / (mui - lmd);
+    let wq = lmd / (mui * (mui - lmd));
+    let l = lmd * w;
+    let lq = lmd * wq;
 
-    return `L= ${l},  Lq= ${lq},  W= ${w},  Wq= ${wq}`;
+    return `L= ${roundToTwo(l)},  Lq= ${roundToTwo(lq)},  W= ${roundToTwo(w)},  Wq= ${roundToTwo(wq)}`;
 }
 
 function calcMM1K(k, lmd, mui) {
@@ -72,8 +83,41 @@ function calcMM1K(k, lmd, mui) {
     return `L= ${l},  Lq= ${lq},  W= ${w},  Wq= ${wq}`;
 }
 
-function calcMMC() {
+function calculate_p0_inverse(lambda, mu, c) {
+    const r = lambda / mu; // Traffic intensity (arrival rate / service rate)
 
+    if (r / c < 1) {
+        // Case 1: r/c < 1
+        let sum1 = 0;
+        for (let n = 0; n < c; n++) {
+            sum1 += Math.pow(r, n) / factorial(n);
+        }
+        const term2 = (c * Math.pow(r, c)) / (factorial(c) * (c - r));
+        return sum1 + term2;
+    } else {
+        // Case 2: r/c >= 1
+        let sum2 = 0;
+        for (let n = 0; n < c; n++) {
+            sum2 += (1 / factorial(n)) * Math.pow(r, n);
+        }
+        const term2 = (1 / factorial(c)) * Math.pow(r, c) * ((c * mu) / (c * mu - lambda));
+        return sum2 + term2;
+    }
+}
+
+function calcMMC(c, lmd, mui) {
+    let r = lmd / mui;
+
+    const numerator = Math.pow(r, c + 1) / c;
+    const denominator = factorial(c) * Math.pow(1 - r / c, 2);
+    let p0 = 1 / calculate_p0_inverse(lmd, mui, c);
+
+    let lq = (numerator / denominator) * p0;
+    let l = lq + r;
+    let wq = lq / lmd;
+    let w = wq + (1 / mui);
+
+    return `L= ${roundToTwo(l)},  Lq= ${roundToTwo(lq)},  W= ${roundToTwo(w)},  Wq= ${roundToTwo(wq)}`;
 }
 
 function calcMMCK(params) {
