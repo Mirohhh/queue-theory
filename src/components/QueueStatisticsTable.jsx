87f4427185e1cdb031391e9362bded3bbba4c544
Mirohhh <omar.h.shabana@gmail.com>
2024-12-09 20:43:54 +0200

    Change button and table styles.

--- src/components/QueueStatisticsTable.jsx
+++ src/components/QueueStatisticsTable.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react';
-import { MM1QueueSimulator } from './MM1QueueSimulator';
 import { MM1QueueSim } from './test';
 
 let result;
 
+/**
+ * QueueStatisticsTable is a React component that takes in lambda, mu, and iterations
+ * as props and displays a table of simulation results. The table includes the
+ * event number, clock time, event type, number of arrivals, number of departures,
+ * number in system, and wait time in the system. The component also contains a
+ * button to run the simulation.
+ *
+ * @param {number} lam - The arrival rate (lambda) of the system.
+ * @param {number} mu - The service rate (mu) of the system.
+ * @param {number} it - The number of simulation iterations.
+ *
+ * @returns {JSX.Element} - A JSX element representing the simulation table
+ *                          component.
+ */
 const QueueStatisticsTable = (props) => {
-  const [arrivalRate, setArrivalRate] = useState('');
-  const [serviceRate, setServiceRate] = useState('');
+  const [arrivalRate, setArrivalRate] = useState(props.lam);
+  const [serviceRate, setServiceRate] = useState(props.mu);
   const [iterations, setIterations] = useState(props.it);
   const [simulationResults, setSimulationResults] = useState([]);
   
@@ -20,26 +33,25 @@ const QueueStatisticsTable = (props) => {
 
   return (
     <div className="p-4">
-      <div className="mb-6 space-y-4">
-        <button 
+      <div className="flex justify-center mb-4">
+        <button
+          className="bg-blue-600 hover:bg-blue-700 transition-colors text-mytext font-semibold text-lg py-2 px-6 border-2 border-btn rounded-md"
           onClick={runSimulation}
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
         >
           Run Simulation
         </button>
       </div>
-
       <div className="overflow-x-auto rounded-md">
         <table className="min-w-full border-primary border-2 rounded-md border-collapse overflow-hidden text-xl">
           <thead className='text-mytext'>
             <tr className="bg-red-600">
-              <th className="border p-2">#Customer</th>
-              <th className="border p-2">Clock</th>
-              <th className="border p-2">Event</th>
-              <th className="border p-2">#Arrival</th>
-              <th className="border p-2">#Departure</th>
-              <th className="border p-2">#InSystem</th>
-              <th className="border p-2">Wait</th>
+              <th className="border py-4 px-6">#Customer</th>
+              <th className="border py-4 px-6">Clock</th>
+              <th className="border py-4 px-6">Event</th>
+              <th className="border py-4 px-6">#Arrival</th>
+              <th className="border py-4 px-6">#Departure</th>
+              <th className="border py-4 px-6">#InSystem</th>
+              <th className="border py-4 px-6">Wait</th>
             </tr>
           </thead>
           <tbody className='text-mytext'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
     extend: {
       colors: {
         mytext: '#F2F2F2',
+        action: 'orangered',
+        btn: 'rgba(255, 255, 255, 0.1)',
         primary: '#212121',
         secondary: '#282828',
       },
